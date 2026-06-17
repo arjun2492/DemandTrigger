fc800be51a6b37a57a8ed7d1ee977522f8bdd565
arjun2492 <arjun2492@gmail.com>
2026-06-17 19:57:00 +0530

    Created IntentIQ database

---
+++ database/schema/01_create_database.sql
@@ -0,0 +1,15 @@
+/*
+==========================================================
+IntentIQ
+Purchase Intent Intelligence Platform
+
+File: 01_create_database.sql
+Description: Creates the IntentIQ database.
+==========================================================
+*/
+
+-- Create Database
+CREATE DATABASE IF NOT EXISTS intentiq;
+
+-- Select Database
+USE intentiq;
